0ae9d01e15ca02ba48bb5582d4afd7b6c039208e
Vitensuu <ulzhanabsattar@gmail.com>
2026-03-31 19:35:44 +0500

    Cерыер

--- core/src/main/java/com/team7/game1/utils/Constants.java
+++ core/src/main/java/com/team7/game1/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.team7.game1.utils;
 
 public class Constants {
-    public static final String SERVER_HOST = "127.0.0.1";
+    public static final String SERVER_HOST = "192.168.201.127";
     public static final int SERVER_PORT = 12345;
 }
