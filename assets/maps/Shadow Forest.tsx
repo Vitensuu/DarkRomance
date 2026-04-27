1f5e1c8d691a098d61387676be557e79e79a90fe
Vitensuu <ulzhanabsattar@gmail.com>
2026-04-27 17:40:21 +0500

    Mixed_metal in characters

---
+++ assets/maps/Shadow Forest.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.1" name="Shadow Forest" tilewidth="86" tileheight="52" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="resources/Shadows_Forest/Sh_tree_oak_3.png" width="74" height="40"/>
+ </tile>
+ <tile id="1">
+  <image source="resources/Shadows_Forest/Sh_bush_1.png" width="22" height="12"/>
+ </tile>
+ <tile id="2">
+  <image source="resources/Shadows_Forest/Sh_bush_2.png" width="28" height="17"/>
+ </tile>
+ <tile id="3">
+  <image source="resources/Shadows_Forest/Sh_bush_3.png" width="37" height="17"/>
+ </tile>
+ <tile id="4">
+  <image source="resources/Shadows_Forest/Sh_bush_4.png" width="46" height="24"/>
+ </tile>
+ <tile id="5">
+  <image source="resources/Shadows_Forest/Sh_bush_5.png" width="38" height="24"/>
+ </tile>
+ <tile id="6">
+  <image source="resources/Shadows_Forest/Sh_bush_6.png" width="41" height="29"/>
+ </tile>
+ <tile id="7">
+  <image source="resources/Shadows_Forest/Sh_tree_oak_2.png" width="82" height="46"/>
+ </tile>
+ <tile id="8">
+  <image source="resources/Shadows_Forest/Sh_tree_oak_1.png" width="86" height="52"/>
+ </tile>
+</tileset>
